25b5abf75a0c118ab7eebdd51b08c1372c0a0d4c
23-Pravallika <learngain23@gmail.com>
2024-02-20 13:28:18 +0530

    AutoPush

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 module "alb-public" {
     source = "./vendor/modules/alb"
     ENV = var.ENV
-    ALB_NAME = robo-${var.ENV}public-alb
+    ALB_NAME = "robo-${var.ENV}-public-alb"
     ALB_INTERNAL = false 
 }
 
 module "alb-private" {
     source = "./vendor/modules/alb"
     ENV = var.ENV
-    ALB_NAME = robo-private-alb
+    ALB_NAME = "robo-${var.ENV}-private-alb"
     ALB_INTERNAL = true  
 }
+
