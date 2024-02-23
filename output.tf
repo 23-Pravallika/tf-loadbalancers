adc226d51946d75adbd390d2f633def649440586
23-Pravallika <learngain23@gmail.com>
2024-02-23 11:36:17 +0530

    AutoPush

--- output.tf
+++ output.tf
@@ -6,3 +6,12 @@ output "PRIVATE_ALB_ARN"{
     value = module.alb-private.ALB_ARN
 }
 
+output "PUBLIC_ALB_INTERNAL"{
+    value = module.alb-public.ALB_INTERNAL
+}
+
+output "PRIVATE_ALB_INTERNAL"{
+    value = module.alb-private.ALB_INTERNAL
+}
+
+
